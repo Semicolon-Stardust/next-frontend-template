72d0fdeb8bf1616255f9f22c22723d27a890b4b8
Neelanjan Mukherji <neelanjanmukherji@gmail.com>
2025-01-23 16:27:47 +0530

    Added Next Config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,22 @@
-import type { NextConfig } from "next";
-import createNextIntlPlugin from "next-intl/plugin";
+import type { NextConfig } from 'next';
+import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
+	images: {
+		domains: ['placehold.co'],
+	},
 	webpack(config) {
 		// Grab the existing rule that handles SVG imports
-		const fileLoaderRule = config.module.rules.find((rule) =>
-			rule.test?.test?.(".svg")
+		const fileLoaderRule: {
+			test?: { test?: (path: string) => boolean };
+			issuer?: { test?: (path: string) => boolean };
+			resourceQuery?: { not?: RegExp[] };
+			exclude?: RegExp;
+		} = config.module.rules.find(
+			(rule: { test?: { test?: (path: string) => boolean } }) =>
+				rule.test?.test?.('.svg')
 		);
 
 		config.module.rules.push(
@@ -22,9 +31,9 @@ const nextConfig: NextConfig = {
 				test: /\.svg$/i,
 				issuer: fileLoaderRule.issuer,
 				resourceQuery: {
-					not: [...fileLoaderRule.resourceQuery.not, /url/],
+					not: [...(fileLoaderRule.resourceQuery?.not || []), /url/],
 				}, // exclude if *.svg?url
-				use: ["@svgr/webpack"],
+				use: ['@svgr/webpack'],
 			}
 		);
 
