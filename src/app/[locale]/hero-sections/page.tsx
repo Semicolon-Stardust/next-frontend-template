c9b82f4deba95800a13a797950f3c8f7b78cee67
aryangulati2k3 <aryangulati2k3@gmail.com>
2025-02-13 15:50:28 +0530

    Updated hero section page

---
+++ src/app/[locale]/hero-sections/page.tsx
@@ -0,0 +1,11 @@
+import AlternateHero from "@/sections/hero-sections/alt-hero-section";
+import Hero from "@/sections/hero-sections/hero-section";
+
+export default function HeroSectionsPage(){
+    return(
+        <div>
+            <Hero/>
+            <AlternateHero/>
+        </div>
+    );
+}
